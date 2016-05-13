c4b618a976700e4458cfa71bca5882449b8b3159
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-05-13 15:31:21 +0600

    run_tests: different bash support

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-bash tests.sh -d tests -s tests/local.setup.sh -d tests/testcases "${@:--A}"
+${BASH:-bash} -c 'echo bash version: $BASH_VERSION'
+${BASH:-bash} tests.sh -d tests -s tests/local.setup.sh -d tests/testcases "${@:--A}"
