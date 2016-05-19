402155bcd6929f12a20df361bbc2564c198e9e73
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2016-05-19 18:34:02 +0600

    remove travis rudiments

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-${BASH:-bash} -c 'echo bash version: $BASH_VERSION'
-${BASH:-bash} tests.sh -d tests -s tests/local.setup.sh -d tests/testcases "${@:--A}"
+./tests.sh -d tests -s tests/setup.sh -d tests/testcases "${@:--A}"
